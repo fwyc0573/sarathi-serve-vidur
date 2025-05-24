ea9ce29165e869051b2e529805e4b8f89f768402
fwyc0573 <935953068@qq.com>
2025-05-25 00:47:27 +0800

    fix

--- vidur/vidur/profiling/prefill_calculation/run_experiments.sh
+++ vidur/vidur/profiling/prefill_calculation/run_experiments.sh
@@ -14,7 +14,7 @@ MODELS=(
 
 for MODEL in "${MODELS[@]}"; do
     echo "Running experiment for model: $MODEL"
-    python prefill_estimation_test.py --model $MODEL --output_dir $OUTPUT_DIR
+    python /research/d1/gds/ytyang/yichengfeng/vidur/sarathi-serve-vidur/vidur/vidur/profiling/prefill_calculation/prefill_estimation_test.py --model $MODEL --output_dir $OUTPUT_DIR
 done
 
 echo "All experiments completed."
